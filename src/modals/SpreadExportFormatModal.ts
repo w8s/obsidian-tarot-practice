02a1900d6760e98d1dc474857cf8bc00ab0fa15c
Todd Waits <todd.waits@gmail.com>
2026-01-25 15:28:54 -0600

    Add SpreadExportFormatModal for export format selection
    Simple modal to ask users if they want to export spread as:
    - JSON only (spread definition)
    - ZIP with template (spread + template.md)
    
    Part of v1.8.1 spread import/export UI.

---
+++ src/modals/SpreadExportFormatModal.ts
@@ -0,0 +1,46 @@
+import { App, Modal } from 'obsidian';
+
+/**
+ * Modal to ask user if they want to include template in spread export
+ */
+export class SpreadExportFormatModal extends Modal {
+	private resolve: (value: boolean) => void;
+	private spreadName: string;
+
+	constructor(app: App, spreadName: string, resolve: (value: boolean) => void) {
+		super(app);
+		this.spreadName = spreadName;
+		this.resolve = resolve;
+	}
+
+	onOpen(): void {
+		const { contentEl } = this;
+		
+		contentEl.createEl('h2', { text: 'Export format' });
+		contentEl.createEl('p', { 
+			text: `The spread "${this.spreadName}" has a custom template. How would you like to export it?` 
+		});
+		
+		const buttonContainer = contentEl.createDiv({ cls: 'modal-button-container' });
+		
+		const zipButton = buttonContainer.createEl('button', { 
+			text: 'ZIP with template', 
+			cls: 'mod-cta' 
+		});
+		zipButton.addEventListener('click', () => {
+			this.resolve(true);
+			this.close();
+		});
+		
+		const jsonButton = buttonContainer.createEl('button', { text: 'JSON only' });
+		jsonButton.addEventListener('click', () => {
+			this.resolve(false);
+			this.close();
+		});
+	}
+
+	onClose(): void {
+		const { contentEl } = this;
+		contentEl.empty();
+	}
+}
